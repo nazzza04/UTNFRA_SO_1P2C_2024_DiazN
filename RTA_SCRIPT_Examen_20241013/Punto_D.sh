20c13230b1d57870a753ecffc5d3335dc241b52c
nazareno <nazaomfg@gmail.com>
2024-10-13 21:06:53 -0400

    arreglo 1

---
+++ RTA_SCRIPT_Examen_20241013/Punto_D.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo mkdir -p EstructuraAsimetrica/correo Estructura_Asimetrica/clientes
+sudo mkdir -p Estructura_Asimetrica/correo/cartas{1..100}
+
+sudo mkdir -p EstructuraAsimetrica/clientes/cartas{1..100}
+
+sudo mkdir -p EstructuraAsimetrica/correo/carteros{1..10}
